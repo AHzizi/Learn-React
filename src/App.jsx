a98781f641849c19692a8f4428d4f09a008bda6b
AHzizi <miftakulazizi0@gmail.com>
2023-10-07 05:41:41 +0700

    fix error

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
 import Form from './components/Form'
-import { Routes, Route } from "react-router-dom";
-import HomePages from './Pages/HomePages';
-import OtherPages from './Pages/OtherPages';
-import TicTacToe from './Pages/GamePages';
+import { Routes, Route } from 'react-router-dom'
+import HomePages from './Pages/HomePages'
+import OtherPages from './Pages/OtherPages'
+import GamePages from './Pages/GamePages' // Corrected import name
+
 function App() {
   const [count, setCount] = useState(0)
-  let a = 0;
   
   return (
-   <div>
-    <Navbar />
-    <Form />
-    <Routes>
-      <Route path='/' Component={HomePages} />
-      <Route path='/Other' Component={OtherPages} />
-      <Route path='Games' Component={GamePages} />
-    </Routes>
-   </div>
+    <div>
+      <Navbar />
+      <Form />
+      <Routes>
+        <Route path="/" element={<HomePages />} />
+        <Route path="/Other" element={<OtherPages />} />
+        <Route path="/Games" element={<GamePages />} /> {/* Corrected component name */}
+      </Routes>
+    </div>
   )
 }
 
