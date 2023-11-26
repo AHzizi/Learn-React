38ef8d300e0411abc30a82e60b2e3a554321c746
AHzizi <miftakulazizi0@gmail.com>
2023-11-26 16:23:13 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,44 @@
-import React, { useState } from 'react'
 import './App.css'
-import Navbar from './components/Navbar'
-import Form from './components/Form'
-import { Routes, Route } from 'react-router-dom'
-import HomePages from './Pages/HomePages'
-import OtherPages from './Pages/OtherPages'
-import GamePages from './Pages/GamePages' // Corrected import name
+import { BsFillMoonStarsFill  } from 'react-icons/bs';
+import {
+  AiFillTwitterCircle,
+  AiFillLinkedin,
+  AiFillFacebook
+} from'react-icons/ai';
+
 
 function App() {
-  const [count, setCount] = useState(0)
   
   return (
-    <div>
-      <Navbar />
-      <Form />
-      <Routes>
-        <Route path="/" element={<HomePages />} />
-        <Route path="/Other" element={<OtherPages />} />
-        <Route path="/Games" element={<GamePages />} /> {/* Corrected component name */}
-      </Routes>
-    </div>
+    <main className='bg-white px-10'>
+      <section className=' min-h-screen'>
+        <nav className='py-10 mb-10 flex justify-between'>
+          <h1 className='text-xl'>LOGO</h1>
+          <ul className='flex items-center'>
+            <li className='cursor-pointer text-xl'>
+              <a href="#">
+                <BsFillMoonStarsFill />
+              </a>
+            </li>
+            <li className='bg-gradient-to-tr from-teal-600 to-cyan-500 text-white px-4 py-2 rounded-md ml-8'>Hi</li>
+          </ul>
+        </nav>
+        <div className='text-center p-10'>
+          <h2 className='text-5xl py-2 text-teal-600 font-medium'>AHzizi</h2>
+          <h3 className='text-2xl py-2'>Exeler</h3>
+          <p className='text-md py-5 leading-8 text-gray-800'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Numquam, dolorem officiis. Quia reprehenderit dicta numquam enim! Dolor officiis quas id.</p>
+
+        </div>
+        <div className='text-5xl flex justify-center gap-16 text-gray-600'>
+          <AiFillTwitterCircle />
+          <AiFillLinkedin />
+          <AiFillFacebook />
+        </div>
+        <div className='relative bg-gradient-to-br from-teal-500 to-cyan-200 rounded-full w-80 h-80 mt-20 mx-auto'>
+          <img src="#" alt="profile" />
+        </div>
+      </section>
+    </main>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,41 +1,9 @@
 import React from 'react'
 
 const Navbar = () => {
-  const nav = [
-    {
-      id: 1,
-      title: 'Home',
-      link: '/'
-    },
-    {
-      id: 2,
-      title: 'Games',
-      link: '/Games'
-    },
-    {
-      id: 3,
-      title: 'Other',
-      link: '/Other'
-    }
-  ];
-
   return (
-    // Your JSX code here, using the 'nav' array if needed.
-    <div>
-      {/* Example usage: */}
-      <ul>
-        {nav.map(item => (
-          <li key={item.id} className='inline ml-5 w-full '>
-            <a href={item.link}>{item.title}</a>
-          </li>
-        )
-          )
-            } 
-              )
-      </ul>
-    </div>
-  );
-};
-
+    <div>Navbar</div>
+  )
+}
 
 export default Navbar
