f985c3bfce2346ca8242187b5e567882d579f006
AHzizi <miftakulazizi0@gmail.com>
2023-11-30 18:42:30 +0700

    learn router

--- src/Pages/HomePages.jsx
+++ src/Pages/HomePages.jsx
@@ -12,16 +12,21 @@ import OtherPages from './OtherPages';
 
 
 const HomePages = () => {
+  const [isDarkMode, setIsDarkMode] = useState(true)
+  const toggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode);
+    document.documentElement.classList.toggle('dark', !isDarkMode);
+  };
   return (
     <div className=''>
-    <main className='bg-white px-10 dark:bg-gray-700'>
+    <main className='bg-white px-10 dark:bg-gray-900'>
       <section className=' min-h-screen'>
         <nav className='py-10 mb-10 flex justify-between'>
-          <h1 className='text-xl'>LOGO</h1>
+          <h1 className='text-xl dark:text-white'>LOGO</h1>
           <ul className='flex items-center'>
             <li className='cursor-pointer text-xl'>
               <a href="#">
-                <BsFillMoonStarsFill onClick={() => setDarkMode(!setDarkMode)} />
+                <BsFillMoonStarsFill onClick={toggleDarkMode} />
               </a>
             </li>
             <li className='bg-gradient-to-tr from-teal-600 to-cyan-500 text-white px-4 py-2 rounded-md ml-8'>Hi</li>
@@ -29,8 +34,8 @@ const HomePages = () => {
         </nav>
         <div className='text-center p-10'>
           <h2 className='text-5xl py-2 text-teal-600 font-medium'>AHzizi</h2>
-          <h3 className='text-2xl py-2'>Exeler</h3>
-          <p className='text-md py-5 leading-8 text-gray-800'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Numquam, dolorem officiis. Quia reprehenderit dicta numquam enim! Dolor officiis quas id.</p>
+          <h3 className='text-2xl py-2 dark:text-white'>Exeler</h3>
+          <p className='text-md py-5 leading-8 text-gray-800 dark:text-white'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Numquam, dolorem officiis. Quia reprehenderit dicta numquam enim! Dolor officiis quas id.</p>
 
         </div>
         <div className='text-5xl flex justify-center gap-16 text-gray-600'>
