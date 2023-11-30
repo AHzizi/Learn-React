e5297546e82b2e30ca30690c21ef86ec95d28c93
AHzizi <miftakulazizi0@gmail.com>
2023-11-30 16:50:07 +0700

    update

--- src/Pages/OtherPages.jsx
+++ src/Pages/OtherPages.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 
-
-const a = false;
 const OtherPages = () => {
   return (
-    <div>OtherPages</div>
+    <main className='group grid grid-rows-4'>
+      <div className='bg-white w-72 h-60 mx-4 my-5 text-center rounded-md group-hover:bg-blue-400'>
+        <h2 className='py-4 text-3xl font-bold group-hover:text-white'><span className='bg-clip-text bg-gradient-to-tr from-teal-500 to-sky-600 text-transparent group-hover:text-white'>Hello</span><span className=' group-hover:text-white bg-gradient-to-tl from-rose-600 to-yellow-400 bg-clip-text text-transparent'> World</span></h2>
+        <p className='px-3 py-9 group-hover:text-white'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia laboriosam totam suscipit enim consectetur iste!</p>
+      </div>
+      <div>
+      <div className='bg-white w-72 h-60 mx-4 my-5 text-center rounded-md group-hover:bg-blue-400'>
+        <h2 className='py-4 text-3xl font-bold group-hover:text-white'><span className='bg-clip-text bg-gradient-to-tr from-teal-500 to-sky-600 text-transparent group-hover:text-white'>Hello</span><span className=' group-hover:text-white bg-gradient-to-tl from-rose-600 to-yellow-400 bg-clip-text text-transparent'> World</span></h2>
+        <p className='px-3 py-9 group-hover:text-white'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia laboriosam totam suscipit enim consectetur iste!</p>
+      </div>
+      </div>
+      
+    </main>
   )
 }
 
