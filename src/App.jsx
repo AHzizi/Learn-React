37a46cb8e0307e0f679e5ff68b91d5ff02a11c22
AHzizi <miftakulazizi0@gmail.com>
2023-12-01 13:02:53 +0700

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import './App.css'
 import { Routes, Route } from "react-router-dom";
 import HomePages  from "./Pages/HomePages";
 import OtherPages from './Pages/OtherPages';
+import AnotherPage from './Pages/AnotherPage';
+
 
 
 
@@ -14,6 +16,7 @@ return (
     <Routes>
       <Route path='/' Component={HomePages} />
       <Route path='/OtherPages' Component={OtherPages} />
+      <Route path='/AnotherPage' Component={AnotherPage} />
     </Routes>
   </div>
   )
---
+++ src/Pages/AnotherPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const AnotherPage = () => {
+  return (
+    <section id='hero' className='min-h-screen h-full flex mx-auto text-center p-3 justify-items-center text-white'>
+        <main>
+            <h4 className='text-2xl'>Kepada Bapak/Ibu/Saudara/i, </h4>
+            <h5 className='text-3xl'>Jonh Doe & Jane Doe</h5>
+            <p>Akan Melaksanakan Pernikahan Pada:</p>
+            <a href="#undangan" className=''>Lihat Undangan</a>
+        </main>
+    </section>
+  )
+}
+
+export default AnotherPage
