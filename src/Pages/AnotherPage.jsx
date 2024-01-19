94890f49196541f1bbb4b8aa1af69ad6f3a597fc
AHzizi <miftakulazizi0@gmail.com>
2024-01-19 07:43:19 +0700

    add Some features

--- src/Pages/AnotherPage.jsx
+++ src/Pages/AnotherPage.jsx
@@ -1,15 +1,13 @@
 import React from 'react'
 
+
 const AnotherPage = () => {
+  let user = "John Doe";
   return (
-    <section id='hero' className='min-h-screen h-full flex mx-auto text-center p-3 justify-items-center text-white'>
-        <main>
-            <h4 className='text-2xl'>Kepada Bapak/Ibu/Saudara/i, </h4>
-            <h5 className='text-3xl'>Jonh Doe & Jane Doe</h5>
-            <p>Akan Melaksanakan Pernikahan Pada:</p>
-            <a href="#undangan" className=''>Lihat Undangan</a>
-        </main>
-    </section>
+  <section className='p-5'>
+    <h1 className='bg-gradient-to-br from-blue-500 to-teal-600 font-bold text-5xl text-transparent bg-clip-text'>Hello World</h1>
+    <h2>${user}</h2>
+  </section>
   )
 }
 
