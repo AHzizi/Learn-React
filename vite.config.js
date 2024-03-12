94ee17005fa0fd1a51b12fc22269bdfd862361f2
AHzizi <miftakulazizi0@gmail.com>
2024-03-12 11:00:25 +0700

    fix it

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// vite.config.js
+export default {
+  build: {
+    assetsInlineLimit: 0,
+    chunkSizeWarningLimit: 500,
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name].[ext]'
+      }
+    }
+  }
+}
+
